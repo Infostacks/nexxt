b0775bc66ba020aadcb4b3b827521af0cea9116b
NomanAfzal1 <nomanafzal262650@gmail.com>
2022-11-01 20:10:55 +0500

    Created the Services Component Folder

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,4 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import img1 from "../../assets/images/images/4.png";
-import img2 from "../../assets/images/images/7.png";
-import img3 from "../../assets/images/images/3.png";
-import FooterApp from "../Footer/Footer";
-import AndroidIcon from "@mui/icons-material/Android";
-import TerminalIcon from "@mui/icons-material/Terminal";
-import AppleIcon from "@mui/icons-material/Apple";
-import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
-import RecipeReviewCard from "../Card/Card";
-import { Box, Button, Typography } from "@mui/material";
 import Projects from "../Projects";
 import Clients from "../Clients";
 import Services from "../Services/Services";
