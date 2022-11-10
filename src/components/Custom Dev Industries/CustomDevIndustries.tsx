35f5d6aeb083a10395d9238f88a483c966de0278
Shak Ahmad <shakeel.ahmad@programmers.io>
2022-11-10 20:22:06 +0500

    set props for intro

--- src/components/Custom Dev Industries/CustomDevIndustries.tsx
+++ src/components/Custom Dev Industries/CustomDevIndustries.tsx
@@ -6,6 +6,7 @@ import {
 } from "../../assets/Strings/Services/customSoftwareDevelopment/customSoftwareDevelopment"; 
 import CustomDevCard from "../Card/CustomDevCard";
 import IntroBanner from "../Global Components/IntroBanner";
+import { introBannerProps } from "../../assets/Strings/Services/customSoftwareDevelopment/customSoftwareDevelopment";
 
 export interface devCardProps {
   devCardDetails: string[];
@@ -26,8 +27,7 @@ const CustomDevIndustries = () => {
     >
       <Grid item md={5}>
         <Box style={{ position: "sticky", top: 80 }}>
-        <IntroBanner heading1="Custom Software Development Services
-for" heading2="Businesses of All Sizes" subtitle1="We create custom, high-impact software solutions that empower businesses and enable them to lead with automation." subtitle2="Our custom software development services provide our partners and clients with the freedom to grow and scale at unprecedented rates. We help startups, medium-sized businesses, and enterprise-grade organizations to build performance-oriented systems." />
+        <IntroBanner {...introBannerProps}/>
         </Box>
       </Grid>
       <Grid md={1}></Grid>
